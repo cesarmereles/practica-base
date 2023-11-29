3769b4d374b949b184d47047b349c4eb8e2cc256
Cesar Augusto Mereles <cesarmereles@gmail.com>
2023-11-29 17:01:45 -0300

    modelo creado y finalizado

--- src/controllers/product-controllers.js
+++ src/controllers/product-controllers.js
@@ -1,10 +1,10 @@
-export const ctrlCreateListProduct = (req,res)=>{}
+import { ProductModel } from "../models/product-model.js";
+export const ctrlCreateListProduct = (req, res) => {};
 
-export const ctrolListProduct = (req,res) => {}
+export const ctrolListProduct = (req, res) => {};
 
-export const ctrolGetProduct =  (req,res) => {}
+export const ctrolGetProduct = (req, res) => {};
 
-export const ctrolUpdateProduct =  (req,res) => {}
+export const ctrolUpdateProduct = (req, res) => {};
 
-export const ctrolDeleteProduct =  (req,res) => {}
-
+export const ctrolDeleteProduct = (req, res) => {};
--- src/models/product-model.js
+++ src/models/product-model.js
@@ -1,1 +1,42 @@
-export const UserModel = {}
+import { Schema, model } from "mongoose";
+
+const ProductSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    maxLength: 50,
+    minLength: 3,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 200,
+    minLength: 3,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["computer", "phone", "accesories", "audio", "videogames"],
+  },
+  stock: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  public: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const ProductModel = model("Product", ProductSchema);
